71bcb52cd3624788f9a1e481748c8f5d349bd4e6
backendverse <225637829+backendverse@users.noreply.github.com>
2025-10-03 10:19:13 +0530

    Initial commit

--- src/main/java/com/kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/kafka/consumer/KafkaConsumer.java
@@ -2,15 +2,20 @@ package com.kafka.consumer;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class KafkaConsumer {
 
-    @KafkaListener(topics = {"order_creation"}, groupId = "payment_group")
+    @KafkaListener(topics = {"spring_topic"},
+//            groupId = "audit_group"
+            topicPartitions = @TopicPartition(topic = "spring_topic", partitions = {"1"})
+    )
     public void orderConsumer(String data) {
-        log.info("Data Consumed For Topic: {}  With Data : {}", "order_creation", data);
+        log.info("Data Consumed For Partition: {}  With Data : {}", "1", data);
     }
+
 
 }
